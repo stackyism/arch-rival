7d9df1d9bca704825f1fbe4035f8822b97e471ca
Saagar Takhi <stakhi08@gmail.com>
2025-03-06 22:44:05 +0100

    feat(tools): implement intersect & union features

--- client/src/workspace/info-tools-panel/info-tools-panel.tsx
+++ client/src/workspace/info-tools-panel/info-tools-panel.tsx
@@ -1,25 +1,46 @@
 import { useSolutionStore } from "../../store.ts";
 import { area } from "@turf/area";
 import { union } from "@turf/union";
+import { intersect } from "@turf/intersect";
 import { featureCollection } from "@turf/helpers";
+import { generateRandomPolygonId } from "../../utils/generateRandomPolygonId.ts";
+import { Polygon } from "@types/geojson";
 
 export const InfoToolsPanel = () => {
-  const { getSelectedPolygons } = useSolutionStore();
+  const { getSelectedPolygons, replacePolygons } = useSolutionStore();
   const selectedPolygons = getSelectedPolygons();
 
-  const unionPolygons = selectedPolygons.length > 1
-    ? union(featureCollection(selectedPolygons))
+  const unionedPolygon = selectedPolygons.length > 1
+    ? union(featureCollection<Polygon>(selectedPolygons))
     : undefined;
 
   const totalArea = area(
-    featureCollection(unionPolygons ? [unionPolygons] : selectedPolygons),
+    featureCollection(unionedPolygon ? [unionedPolygon] : selectedPolygons),
   );
+
+  const handleIntersectPolygons = () => {
+    const intersectedPolygon = intersect(featureCollection(selectedPolygons));
+    if (intersectedPolygon) {
+      replacePolygons({ ...intersectedPolygon, id: generateRandomPolygonId() });
+    }
+  };
+
+  const handleUnionPolygons = () => {
+    if (unionedPolygon) {
+      replacePolygons(
+        {
+          ...unionedPolygon,
+          id: generateRandomPolygonId(),
+        },
+      );
+    }
+  };
 
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <button>Intersect</button>
-        <button>Union</button>
+        <button onClick={handleIntersectPolygons}>Intersect</button>
+        <button onClick={handleUnionPolygons}>Union</button>
       </div>
       {totalArea
         ? (
